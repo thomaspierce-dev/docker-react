51bb20718731898f17c7e74c1fe282ca4f65815e
tpierce-dev <thecodegetter@gmail.com>
2019-01-31 21:28:07 -0500

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            I was changed!!!
+            Greetings Joe Boye and Kevin Xie!
           </p>
           <a
             className="App-link"
